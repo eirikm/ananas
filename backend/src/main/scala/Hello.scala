e75574ceeb2dd04e8f15ace2093b07422db85ccd
Eirik Meland <eirik.meland@arktekk.no>
2016-12-29 18:51:41 +0100

    første versjon av regnestykkegenering

--- backend/src/main/scala/Hello.scala
+++ backend/src/main/scala/Hello.scala
@@ -1,10 +1,39 @@
 import unfiltered.request._
 import unfiltered.response._
 
+import scala.util.Random
+
+import io.circe._, io.circe.generic.semiauto._
+import io.circe.syntax._
+
+
 object Hello extends App {
+  import JsonCodecs._
+
   val plan = unfiltered.filter.Planify {
-    case Path(Seg(Nil)) => ResponseString("Hello")
+    case Path(Seg(Nil))             => ResponseString("Hello")
+    case Path(Seg("pluss" :: Nil))  => ResponseString(GenererMatteStykker.genererRegnestykke(vanskelighetsGrad = 1).asJson.spaces2)
   }
 
   unfiltered.jetty.Server.local(1337).plan(plan).run()
 }
+
+object GenererMatteStykker extends App {
+  def genererRegnestykke(vanskelighetsGrad: Int) = {
+    val a = Random.nextInt(5)
+    val b = Random.nextInt(4)
+
+    Regnestykke(a, b, "+")
+  }
+
+  val regnestykke = genererRegnestykke(0)
+
+  import JsonCodecs._
+  println(regnestykke.asJson)
+}
+
+object JsonCodecs {
+  implicit val regnestykkeEncoder: Encoder[Regnestykke] = deriveEncoder
+}
+
+case class Regnestykke(a: Int, b: Int, op: String)
